9527d8d2cc9ca85060cdde5f9aad7716351eb9cc
SouhaiBusiness <i79916475@gmail.com>
2025-02-01 18:33:05 +0100

    sync clerck2

--- src/app/api/webhooks/route.ts
+++ src/app/api/webhooks/route.ts
@@ -1,7 +1,7 @@
 import { Webhook } from 'svix';
 import { headers } from 'next/headers';
 //import { createOrUpdateUser, deleteUser } from '@/lib/actions/user';
-import { clerkClient } from '@clerk/nextjs/server';
+//import { clerkClient } from '@clerk/nextjs/server';
 
 export async function POST(req) {
   // You can find this in the Clerk Dashboard -> Webhooks -> choose the endpoint
@@ -56,7 +56,7 @@ export async function POST(req) {
   console.log(`Webhook with and ID of ${id} and type of ${eventType}`);
   console.log('Webhook body:', body);
 
-  if (eventType === 'user.created' || eventType === 'user.updated') {
+ {/* if (eventType === 'user.created' || eventType === 'user.updated') {
     const { id, first_name, last_name, image_url, email_addresses, username } =
       evt?.data;
     try {
@@ -98,7 +98,7 @@ export async function POST(req) {
         status: 400,
       });
     }
-  }
+  }*/}
 
   return new Response('', { status: 200 });
 }
